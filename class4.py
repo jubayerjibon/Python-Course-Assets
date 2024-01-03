ca41da327f603f5b6f13251c08b774eccc797033
jubayerjibon <60441326+jubayerjibon@users.noreply.github.com>
2024-01-03 10:43:37 +0600

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,101 @@
+'''
+integer
+float
+string
+boolean
+list
+dictionary
+tuple
+set
+# Data Types
+mutable:
+list, dictionary, byte array
+
+immutable:
+int, float, complex, string, tuple, set
+'''
+
+# a = 5
+# b = 5.5
+# print(type(b))
+# c = "Jibon"
+# f ='Jubayer'
+
+# d = True
+
+# name = "Jubayer Hossen Jibon"
+# print(name)
+
+# info = '''
+# Name: Jubayer Hossen Jibon
+# Age: 21
+# Gender: Male
+# Course: Python
+# '''
+# print(info)
+# print(type(info))
+
+# '''
+# # complex number:
+# # x + yi
+#   10 + 2i
+#   i = root(-1)
+
+
+# '''
+
+# com = 10 + 2j
+# print(com)
+# print(type(com))
+
+# list
+# '''
+# list is an orderd sequence of items.
+# flexible.
+# ["Jibon", 21, 3.95, 'Jubayer']
+#     0      1      2       3
+# '''
+# li = ["Jibon", 21, 3.95, 'Jubayer']
+# print(li)
+# print(type(li))
+# print(li[2])
+# print(li[1])
+
+# tuple
+'''
+tuple is an ordered sequence of items same as a list but immutable.
+'''
+# li = ["Jibon", 21, 3.95, 'Jubayer']
+# tp = ("Python", 3, 'Class-4')
+
+# print(li)
+# li[1] = 10
+# print(li)
+# print(tp)
+# tp[1] = 20
+# print(tp)
+
+
+# Dictionary -->
+'''
+it is an unordered collection of key-value pairs.
+defined within {} with each item being a pair in the form {key:value}
+'''
+# dc = {1:"Jibon", 2:"Python", 3:"Class-4"}
+# print(dc)
+# print(dc[1])
+# print(dc[3])
+
+# dc = {1:"Jibon", "age":21, "course":"Python", "gender":"Male"}
+# print(dc)
+# print(dc["age"])
+
+set = {8, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(set)
+
+
+
+
+
+
+
